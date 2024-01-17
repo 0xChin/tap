13f815c5d6780bd28ac449f41e7d6ef9ea0d8373
Alan Racciatti <alanracciatti1220@gmail.com>
2024-01-17 03:28:36 -0300

    refactor(api): use next.js api instead of express backend

---
+++ config.ts
@@ -0,0 +1,21 @@
+import { createPublicClient, createWalletClient, custom, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+
+export const publicClient = createPublicClient({
+  chain: sepolia,
+  transport: http(
+    "https://eth-sepolia.g.alchemy.com/v2/2_Fhzc4WRgdlybIVlDRn9akE4dH3I_Lp"
+  ),
+});
+
+export const walletClient = createWalletClient({
+  chain: sepolia,
+  transport: http(
+    "https://eth-sepolia.g.alchemy.com/v2/2_Fhzc4WRgdlybIVlDRn9akE4dH3I_Lp"
+  ),
+});
+
+export const account = privateKeyToAccount(
+  process.env.PRIVATE_KEY as `0x${string}`
+);
